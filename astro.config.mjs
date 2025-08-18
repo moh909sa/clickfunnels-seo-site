a4fffcce833e19b7650058691f36a30fe6fff06a
moh909sa <moh.saeed.shbnan997@gmail.com>
2025-08-18 19:59:41 +0300

    fix: add @astrojs/vercel adapter to resolve Vercel deployment issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://clickfunnels-seo-site.vercel.app',
+  output: 'static',
+  adapter: vercel(),
   integrations: [mdx(), tailwind()],
   markdown: {
     shikiConfig: {
